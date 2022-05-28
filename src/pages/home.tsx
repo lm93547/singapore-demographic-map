27191f22531940f80868d41346d6d27819fc90d4
Lucas Martin <lucasmartin045@gmail.com>
2022-05-28 13:43:33 -0400

    adding nav bar

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import { Box, useColorModeValue,  } from '@chakra-ui/react';
+import Nav from '../components/NavBar/index';
 
 type Props = {}
 
 const Home = (props: Props) => {
   return (
-    <div>Home</div>
+    <Box w='100vw' h="100vh" bg={useColorModeValue('gray.400', 'gray.800')}>
+        <>
+            <Nav />
+        </>
+    </Box>
   )
 }
 
