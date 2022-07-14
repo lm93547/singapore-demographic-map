d9f5cb6c8abcd063637bf85f0757eb3eb5a40580
Lucas Martin <lucasmartin045@gmail.com>
2022-07-14 20:13:16 +0100

    MRT Line colours constant

---
+++ src/utils/constants/mrtLineColours.ts
@@ -0,0 +1,9 @@
+export const mrtLineColours: any = {
+    "orange": "#ffa500",
+    "darkslateblue": "#483d8b",
+    "mediumseagreen": "#3cb371",
+    "darkmagenta": "#8b008b",
+    "orangered": "#ff4500",
+    "saddlebrown": "#8b4513",
+    "gray": "#808080",
+}
