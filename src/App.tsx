8816ccd9a465293ac4a69987e72a1b0233ee2793
Lucas Martin <lucasmartin045@gmail.com>
2022-07-14 20:23:53 +0100

    useEffects in App to auth network calls

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,46 @@ import { ChakraProvider } from '@chakra-ui/react'
 import theme from "./styles/theme";
 import About from './pages/about';
 import Home from './pages/home';
+import 'maplibre-gl/dist/maplibre-gl.css';
+import { Provider } from "mobx-react";
+import store from "./store";
+import {useEffect, useState} from 'react';
+import { authenticateApi } from './utils/API/authenticateApi';
+import Fonts from "./styles/Fonts";
+import { dataFetching } from "./utils/API/dataFetching";
 
 function App() {
+    const [oneMapToken, setOneMapToken] = useState('')
+
+    const response = async () => {
+        const allRegionArray = await dataFetching('getRegions', oneMapToken)
+        store.map.setRegions(allRegionArray)
+    }
+
+    useEffect(()=>{
+        authenticateApi('onemap').then((res:any) => {setOneMapToken(res.access_token)});
+    }, [])
+
+    useEffect(() => {
+        store.auth.setTokens({onemap: oneMapToken, ura: ''});
+        if(oneMapToken !== ""){
+            response()
+        }
+    }, [oneMapToken])
+
+    
   return (
-    <ChakraProvider theme={theme}>
-        <BrowserRouter>
-            <Routes>
-                <Route index element={<Home />} />
-                <Route path='/about' element={<About />} />
-            </Routes>
-        </BrowserRouter>
-    </ChakraProvider>
+    <Provider store={store} >
+        <ChakraProvider theme={theme}>
+            <Fonts />
+            <BrowserRouter>
+                <Routes>
+                    <Route index element={<Home />} />
+                    <Route path='/about' element={<About />} />
+                </Routes>
+            </BrowserRouter>
+        </ChakraProvider>
+    </Provider>
   );
 }
 
