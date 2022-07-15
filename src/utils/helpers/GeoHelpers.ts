cd8a72820cdcf7183e4da0a8b27749e9adb4352e
Lucas Martin <lucasmartin045@gmail.com>
2022-07-15 23:11:55 +0100

    Calc Time Geohelper

--- src/utils/helpers/GeoHelpers.ts
+++ src/utils/helpers/GeoHelpers.ts
@@ -2,11 +2,19 @@ const GeoHelpers = {
     getPropertyFromGeoJson: function getPropertyFromGeoJson(json: any, property: string, type: string, featureProperty: string, propertyMatcher: string) {
         const result = json[property].filter((feature: any)=>{
             if(feature.geometry.type === type && feature.properties[featureProperty] === propertyMatcher){
-                console.log('point', feature.properties.stop_type)
                 return feature
             }
         })
         return result;
+    },
+    calcTime: function calcTime(offset: string) {
+        const date = new Date();
+        const UTC = date.getTime() + (date.getTimezoneOffset() * 60000);
+
+        const newDate = new Date(UTC + (3600000*Number(offset)));
+    
+        return newDate.toLocaleString();
+    
     }
 }
 
