34083fc230ea68807dcac5b453d59c7e3c346f6a
Lucas Martin <lucasmartin045@gmail.com>
2022-07-15 23:12:10 +0100

    get weather api call

--- src/utils/API/dataFetching.ts
+++ src/utils/API/dataFetching.ts
@@ -1,10 +1,11 @@
 import ky from 'ky'
 
 export const urlToMethodMapping = {
-    getRegions: 'https://developers.onemap.sg/privateapi/popapi/getAllPlanningarea?token='
+    getRegions: 'https://developers.onemap.sg/privateapi/popapi/getAllPlanningarea?token=',
+    getWeather: "https://api.open-meteo.com/v1/forecast?latitude=1.2894&longitude=103.8500&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_sum,rain_sum,showers_sum,precipitation_hours&timezone=Asia%2FSingapore"
 }
 
-export const dataFetching = async (type: "getRegions", token: string) => {
+export const dataFetching = async (type: "getRegions" | "getWeather", token: string = "") => {
     if(type === "getRegions"){
         let url = urlToMethodMapping[type];
         url += token;
@@ -12,5 +13,10 @@ export const dataFetching = async (type: "getRegions", token: string) => {
         const regionResponse = await ky.get(url).json();
         console.log("🚀 ~ file: dataFetching.ts ~ line 13 ~ dataFetching ~ regionResponse", regionResponse)
         return regionResponse;
+    }
+    if(type === "getWeather"){
+        let url = urlToMethodMapping[type];
+        const weatherResponse = await ky.get(url).json();
+        return weatherResponse;
     }
 }
