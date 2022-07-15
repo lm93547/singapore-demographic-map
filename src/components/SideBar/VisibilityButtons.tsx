62553be332986f51311a777b93e5e903c95c925c
Lucas Martin <lucasmartin045@gmail.com>
2022-07-15 23:11:24 +0100

    test id for vis buttons

--- src/components/SideBar/VisibilityButtons.tsx
+++ src/components/SideBar/VisibilityButtons.tsx
@@ -7,19 +7,19 @@ type Props = {}
 
 const VisibilityButtons = (props: Props) => {
     return (
-        <Flex direction="column" >
-            <Button lineHeight="unset" display='flex' alignItems='center' bgSize='175%' backdropFilter={'opacity(0.2)'} bgImage={'/assets/images/contours.jpg'} colorScheme='blue' variant={store.map.layerVis.mrt ? "outline" : "ghost"} mb="4" onClick={() => {
+        <Flex data-testid="vis-btns" direction="column" >
+            <Button transition="1s all" borderBottom="dotted" border="none" lineHeight="unset" display='flex' alignItems='center' bgSize='175%' backdropFilter={'opacity(0.2)'} colorScheme='blue' variant={store.map.layerVis.mrt ? "solid" : "outline"} mb="4" onClick={() => {
                 store.map.setVis('mrt', !store.map.layerVis.mrt)
                 store.map.setSidebarOpen('right', !store.map.sidebar.right)
             }}>
                 <Text lineHeight="unset" fontFamily="Coalition" >MRT</Text>
             </Button>
-            <Button lineHeight="unset" bgSize='175%' backdropFilter={'opacity(0.2)'} bgImage={'/assets/images/contours.jpg'} colorScheme='blue' variant={store.map.layerVis.pcn ? "outline" : "ghost"} mb="4" onClick={() => {
+            <Button transition="1s all" borderBottom="dotted" border="none" lineHeight="unset" bgSize='175%' backdropFilter={'opacity(0.2)'} colorScheme='blue' variant={store.map.layerVis.pcn ? "solid" : "outline"} mb="4" onClick={() => {
                 store.map.setVis('pcn', !store.map.layerVis.pcn)
             }}>
                 <Text lineHeight="unset" fontFamily="Coalition" >PCN</Text>
             </Button>
-            <Button lineHeight="unset" bgSize='175%' backdropFilter={'opacity(0.2)'} bgImage={'/assets/images/contours.jpg'} colorScheme='blue' variant={store.map.layerVis.region ? "outline" : "ghost"} mb="4" onClick={() => {
+            <Button transition="1s all" borderBottom="dotted" border="none" lineHeight="unset" bgSize='175%' backdropFilter={'opacity(0.2)'} colorScheme='blue' variant={store.map.layerVis.region ? "solid" : "outline"} mb="4" onClick={() => {
                 store.map.setVis('region', !store.map.layerVis.region)
             }}>
                 <Text lineHeight="unset" fontFamily="Coalition" >Regions</Text>
