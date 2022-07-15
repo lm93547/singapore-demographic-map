ac1382f3055e9e4895ac21ece18a0a66b6314794
Lucas Martin <lucasmartin045@gmail.com>
2022-07-15 23:12:25 +0100

    remove logs

--- src/utils/API/authenticateApi.ts
+++ src/utils/API/authenticateApi.ts
@@ -2,8 +2,6 @@ import ky from 'ky';
 
 export const authenticateApi = async (type: 'onemap' | 'ura') => {
     if(type === 'onemap'){
-        console.log(process.env.REACT_APP_EMAIL);
-        console.log(process.env.REACT_APP_PASSWORD);
         const token = 
             await ky.post(
                 'https://developers.onemap.sg/privateapi/auth/post/getToken', 
