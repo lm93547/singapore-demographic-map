cf5759b6c1b8813c4bb0df31b58b91626d531bb9
Lucas Martin <lucasmartin045@gmail.com>
2022-07-30 11:32:09 +0100

    adding other sidebar for extra info

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -7,6 +7,7 @@ import VisibilityButtons from '../components/SideBar/VisibilityButtons';
 import store from '../store/index';
 import StationList from '../components/MRTComponents/StationList';
 import { observer } from 'mobx-react';
+import SidebarHeading from '../components/SideBar/SidebarHeading';
 
 type Props = {}
 
@@ -14,20 +15,23 @@ const Home = (props: Props) => {
     const smVariant = { navigation: 'drawer', navigationButton: true }
     const mdVariant = { navigation: 'sidebar', navigationButton: false }
     const variants = useBreakpointValue({ base: smVariant, md: mdVariant })
-    const { isOpen, onOpen, onClose } = useDisclosure()
+    const { onClose } = useDisclosure()
 
-    const toggleSidebar = () => store.map.setSidebarOpen("left", !store.map.sidebar.left)
-    const toggleSidebarRight = () => store.map.setSidebarOpen("right", !store.map.sidebar.right)
 
     return (
         <Box w='100vw' h="100vh" bg={useColorModeValue('gray.400', 'gray.800')}>
             <>
-                <Nav onShowSidebar={toggleSidebar} />
+                <Nav />
             </>
             <Sidebar
                 variant={variants?.navigation}
                 isOpen={store.map.sidebar.left}
-                desktopComponent={<VisibilityButtons />}
+                desktopComponent={
+                    <>
+                        <SidebarHeading />
+                        <VisibilityButtons />                    
+                    </>
+                }
                 mobileComponent={<></>}
                 side="left"
                 onClose={onClose}
