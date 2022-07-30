b82690af726b70f20425153280468b550b37e2e9
Lucas Martin <lucasmartin045@gmail.com>
2022-07-30 11:35:45 +0100

    Adding jest config

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,16 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jest-environment-jsdom',
-  coverageReporters: ["json-summary", "lcov", "text-summary", "text", "html"],
-	collectCoverage: false,
-	collectCoverageFrom: [
-		"<rootDir>/src/**/*.{ts,tsx}",
+    preset: 'ts-jest/presets/js-with-ts',
+    testEnvironment: 'jest-environment-jsdom',
+    coverageReporters: ["json-summary", "lcov", "text-summary", "text", "html"],
+    collectCoverage: false,
+    collectCoverageFrom: [
+        "<rootDir>/src/**/*.{ts,tsx}",
+    ],
+    "transform": {
+        "^.+\\.(ts|tsx)$": "ts-jest"
+    },
+    "transformIgnorePatterns": [
+        "node_modules/(?!ky/.*)"
     ],
 };
